0c0636aafb03a4a2cf967371f7e012777be7a797
anemadhon <anemadhon@gmail.com>
2022-11-06 03:05:08 +0700

    initial migration

---
+++ database/migrations/2022_11_05_194703_create_bills_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('name');
+            $table->enum('status', ['paid', 'unpaid']);
+            $table->decimal('amount');
+            $table->decimal('tax_or_other_cost');
+            $table->decimal('discount');
+            $table->dateTimeTz('due_date');
+            $table->dateTimeTz('paid_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+};
