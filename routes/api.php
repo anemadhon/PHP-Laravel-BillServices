eddc127a21f98933c0b3cab46d3c950304f6b3d3
anemadhon <anemadhon@gmail.com>
2022-11-06 10:00:17 +0700

    add route for authentication

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::apiResource('bills', \App\Http\Controllers\BillController::class)->only(['index', 'show', 'store']);
-Route::put('payments/{payment}', \App\Http\Controllers\PaymentController::class)->name('payments.update');
+Route::post('authentications', [\App\Http\Controllers\AuthenticationController::class, 'login'])->name('auth.login');
+Route::put('authentications/{id}', [\App\Http\Controllers\AuthenticationController::class, 'logout'])->name('auth.logout');
+Route::group([
+    'middleware' => \App\Http\Middleware\IsLogin::class
+], function () {
+    Route::apiResource('bills', \App\Http\Controllers\BillController::class)->only(['index', 'show', 'store']);
+    Route::put('payments/{payment}', \App\Http\Controllers\PaymentController::class)->name('payments.update');
+});
