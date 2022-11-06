fa51d8d56781f92a035a6223a7a2633953da64ee
anemadhon <anemadhon@gmail.com>
2022-11-06 10:00:05 +0700

    add simple middleware islogin

---
+++ app/Http/Middleware/IsLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\UserLogin;
+use Closure;
+use Illuminate\Http\Request;
+
+class IsLogin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $loggedInUser = UserLogin::where('user_id', $request->user_id)->first();
+        
+        if (!$loggedInUser || ($loggedInUser && !$loggedInUser->is_login)) {
+            abort(401, 'Unauthorized');
+        }
+
+        return $next($request);
+    }
+}
